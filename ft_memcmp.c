0ac750b687d7e21272210269ce68fcef7c7e03e1
Carmen Gomez Rodriguez <cagomez-@c2r6s5.42madrid.com>
2024-10-02 20:55:55 +0200

    3 commit

---
+++ ft_memcmp.c
@@ -0,0 +1,19 @@
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*s11;
+	unsigned char	*s12;
+	size_t			i;
+
+	s11 = (unsigned char *) s1;
+	s12 = (unsigned char *) s2;
+
+	while ((s11[i] || s12[i]) && i < n)
+}
+
+
+int main(void)
+{
+    s1[] = "Hello World";
+    s2[] = "HeLlo WorLd";
+    printf("La cadena devuelve%s", memcmp )
+}
---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cagomez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/26 18:07:54 by cagomez-          #+#    #+#             */
+/*   Updated: 2024/09/26 18:37:15 by cagomez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
+/*
+void *memmove(void *dest, const void *src, size_t n)
+{
+}
+*/
+int main(void)
+{
+	printf("tamaño de tipo%ld\n", sizeof(void));
+	return (0);
+}
