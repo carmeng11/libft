2c9f48b84f03e7e335d76e6db7499171ea3e738b
Carmen Gomez Rodriguez <cagomez-@c2r6s5.42madrid.com>
2024-10-04 20:46:03 +0200

    3_____

---
+++ ft_memcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cagomez- <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/04 20:27:48 by cagomez-          #+#    #+#             */
+/*   Updated: 2024/10/04 20:27:52 by cagomez-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	unsigned char	*s11;
+	unsigned char	*s12;
+	size_t			i;
+
+	s11 = (unsigned char *) s1;
+	s12 = (unsigned char *) s2;
+
+	while ((s11[i] || s12[i]) && i < n)
+}
+
+
+int main(void)
+{
+    s1[] = "Hello World";
+    s2[] = "HeLlo WorLd";
+    printf("La cadena devuelve%s", memcmp )
+}
