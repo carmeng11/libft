9c8c18cde1c0ada1e7c85970a44027b3813a11fd
Carmen Gomez Rodriguez <cagomez-@c2r6s6.42madrid.com>
2024-10-17 20:44:37 +0200

    con split

--- libft.h
+++ libft.h
@@ -49,5 +49,6 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_strmapi(const char *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 char	*ft_itoa(int n);
+char	**ft_split(char const *s, char c);
 
 #endif
